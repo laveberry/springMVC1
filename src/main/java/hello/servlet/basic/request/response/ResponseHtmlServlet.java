decc405a9ae85a0a2b2e73c3e88b6a5cefcb12f1
laveberry <laveberry7@gmail.com>
2022-12-01 06:08:37 +0900

    HTTP응답데이터 - 단순텍스트, HTML

---
+++ src/main/java/hello/servlet/basic/request/response/ResponseHtmlServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic.request.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//서블릿으로 html생성
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type : text/html; charset=utf-8
+        response.setContentType("text/html"); //html렌더링 위함
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+}
