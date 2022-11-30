1b6c7260bd3407d02ca95f82497f0a3899b38286
laveberry <laveberry7@gmail.com>
2022-12-01 06:19:26 +0900

    HTTP응답데이터 - API JSON

---
+++ src/main/java/hello/servlet/basic/request/response/ResponseJsonServlet.java
@@ -0,0 +1,37 @@
+package hello.servlet.basic.request.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//HTTP응답데이터 JSON으로 뿌리기
+@WebServlet(name="responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    //Content-type : application/json
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type : application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //스프링에서는 이코드만 추가해주면됨(하위생략가능)
+        //return helloData;
+
+        //{"username":"kim", "age":20}
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
