efb95b965c5585f16079efaa358455bed8051a86
laveberry <laveberry7@gmail.com>
2023-01-19 20:24:40 +0900

    View분리 - V2

---
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,55 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// /front-controller/v2/members/new-form
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    //컨트롤 엔터 : 빈생성자 만들어서 작성
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+        //  /front-controller/v1/members
+        String requestURI = request.getRequestURI();
+
+        //다형성 적용
+        ControllerV2 controller = controllerMap.get(requestURI);
+
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); //404
+            return;
+        }
+        //조회 후 인터페이스 호출
+        //new Myview("/WEB-INF/views/new-form.jsp")
+        //커맨드 옵션후 process클릭시 구현 여러개보임
+        MyView view = controller.process(request, response);
+        view.render(request, response);
+
+
+    }
+}
